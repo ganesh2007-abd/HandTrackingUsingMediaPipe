19d1a90ce7180b1726227732f6e1b4de08e180d3
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-05-15 19:35:14 +0530

    Create FPSHandDrawing.py
    Dont forget to typecast fps to string after rounding to int

---
+++ FPSHandDrawing.py
@@ -0,0 +1,39 @@
+import cv2 as cv
+import mediapipe as mp
+import time 
+
+cap= cv.VideoCapture(0)
+
+mphands = mp.solutions.hands
+hands = mphands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+ctime = 0
+ptime=0
+
+while True:
+    success,img = cap.read()
+
+    if not success:
+        break
+
+    imgRGB = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+
+    ctime = time.time()
+    fps = 1/(ctime - ptime)
+    ptime = ctime
+
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handlmk in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img,handlmk,mphands.HAND_CONNECTIONS)
+
+    cv.putText(img,str(int(fps)),(10,70),cv.FONT_HERSHEY_COMPLEX,3.0,(255,0,255),2)
+
+    cv.imshow("Image",img)
+    if cv.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
